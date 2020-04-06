4b6ae02ab1edae73e1c6d1abe93a7c3703479923
Catastrophe0123 <vnavaneeth15@gmail.com>
2020-04-06 10:43:23 +0530

    added registration

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const http = require('http');
-// const axios = require('axios');
 const app = express();
 const socketIo = require('socket.io');
+const axios = require('axios').default;
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('error', function (error) {
+	console.error(error);
+});
+
+client.on('connect', function () {
+	console.log('redis connected');
+});
 
 const port = process.env.PORT || 4001;
 
@@ -18,7 +28,48 @@ io.on('connection', (socket) => {
 	// here you can start emitting events to the client
 	console.log('new client connected');
 
-	socket.emit('hello', 'hello world');
+	socket.on('registerUser', (username) => {
+		console.log(username);
+		client.sadd(['users', username], function (err, reply) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('redis thithg : ', reply);
+				// 1 if inserted successfully
+				// 0 if duplicate value
+				if (reply === 0) {
+					socket.emit('USERNAME_EXISTS');
+				} else {
+					socket.emit('REGISTER_SUCCESS');
+				}
+			}
+		});
+
+		// REMOVING VALUES
+		// client.srem('users', 'nava', function (err, reply) {
+		// 	if (err) {
+		// 		console.error(err);
+		// 	} else {
+		// 		console.log('removed nava : ', reply);
+		// 	}
+		// });
+
+		// FETCHING VALUES
+		client.smembers('users', function (err, reply) {
+			if (err) {
+				console.error(err);
+			} else {
+				console.log('all values stored in this : ', reply);
+			}
+		});
+	});
+
+	socket.on('newGame', async () => {
+		let response = await axios.get(
+			'https://cards-against-humanity-api.herokuapp.com/sets/Base'
+		);
+		socket.emit('sets', response.data);
+	});
 
 	socket.on('disconnect', () => {
 		console.log('Client disconnected');
--- frontend/cards-against-humanity/src/App.js
+++ frontend/cards-against-humanity/src/App.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
 import socketIOClient from 'socket.io-client';
 
+const endpoint = 'http://127.0.0.1:4001';
+const socket = socketIOClient(endpoint);
+
 class App extends Component {
-	state = { data: null };
+	state = { data: null, username: '', errors: null };
 
 	componentDidMount() {
-		const endpoint = 'http://127.0.0.1:4001';
-		const socket = socketIOClient(endpoint);
-		socket.on('hello', (data) => this.setState({ data: data }));
+		socket.emit('newGame');
+		socket.on('sets', (data) => console.log(data));
+		socket.on('USERNAME_EXISTS', () => {
+			this.setState({ errors: 'username currently in use' });
+		});
+		socket.on('REGISTER_SUCCESS', () => {
+			this.setState({ errors: null });
+			// TODO : redirect to rooms page
+		});
 	}
+
+	onChangeHandler = (event) => {
+		this.setState({
+			username: event.target.value,
+		});
+	};
+
+	onSubmitHandler = () => {
+		socket.emit('registerUser', this.state.username);
+	};
 
 	render() {
 		return (
 			<div>
-				hello worlds
-				<div>{this.state.data}</div>
+				<h1>WELCOME TO CARDS AGAINST HUMANITY</h1>
+				<br />
+				<label htmlFor='name'>
+					PLEASE ENTER A UNIQUE USERNAME :{' '}
+					<input
+						value={this.state.username}
+						onChange={this.onChangeHandler}
+						type='text'
+						name='name'
+						placeholder='Enter a username'
+					/>
+				</label>{' '}
+				<button onClick={this.onSubmitHandler}>SUBMIT</button>
+				{this.state.errors ? <p>{this.state.errors}</p> : null}
 			</div>
 		);
 	}
